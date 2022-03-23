08f0c811d50900c59fd02a53720eb354394b14b5
DELL <DELL@SEMIH>
2022-03-23 18:57:47 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+        //İlişkili tablolarda kolon silmek hatalara sebep olur. o yüzden category'i aktif pasif durumuna getireceğiz.
+
+    }
+}
