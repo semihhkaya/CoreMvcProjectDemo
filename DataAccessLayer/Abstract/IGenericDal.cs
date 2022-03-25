9fd2ac58843984b9767ce445b9eae55949ec2a76
DELL <DELL@SEMIH>
2022-03-25 09:51:13 +0300

    Generic Repository. Entity CRUD.

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T:class //T crud operasyonlarını uygulayacağımız entitymize (vt nesnesi) karşılık geliyor.
+    {
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetListAll();
+        T GetById(int id);
+    }
+}
---
+++ DataAccessLayer/Repositories/BlogRepository.cs
@@ -0,0 +1,48 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class BlogRepository : IBlogDal
+    {
+        public void AddBlog(Blog blog)
+        {
+            //Using kullanımı ile Context context = new Context(); kullanımı arasında fark vardır. Using kullanımı ile beraber kod bloğu tamamlandığında ıdısopable interfacenin disopable fonksiyonu çalışır ve kod elden çıkarılır. sonlandırılır. Context türünden türetilirse bu işlem gerçeklşemez.
+            using var c = new Context();
+            c.Add(blog);
+            c.SaveChanges();
+        }
+
+        public void DeleteBlog(Blog blog)
+        {
+            using var c = new Context();
+            c.Remove(blog);
+            c.SaveChanges();
+        }
+
+        public Blog GetById(int id)
+        {
+            using var c = new Context();
+            return c.Blogs.Find(id);
+        }
+
+        public List<Blog> ListAllBlog()
+        {
+            using var c = new Context();
+            return c.Blogs.ToList();
+        }
+
+        public void UpdateBlog(Blog blog)
+        {
+            using var c = new Context();
+            c.Update(blog);
+            c.SaveChanges();
+        }
+    }
+}
