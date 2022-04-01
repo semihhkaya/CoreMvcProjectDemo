ad73cbc2ea31b95cea829913ef05f99499b32326
DELL <DELL@SEMIH>
2022-04-02 00:59:55 +0300

    View Component

---
+++ CoreMvcProjectDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,35 @@
+﻿using CoreMvcProjectDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMvcProjectDemo.ViewComponents
+{
+    public class CommentList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    Id=1,
+                    UserName="Semih"
+                },
+                new UserComment
+                {
+                    Id=2,
+                    UserName="Kaya"
+                },
+                new UserComment
+                {
+                    Id=3,
+                    UserName="Deneme"
+                }
+            };
+            return View(commentvalues);
+        }
+    }
+}
