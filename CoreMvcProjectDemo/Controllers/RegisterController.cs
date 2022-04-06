a6ff8bb6b44d92298d202b541773b4a860385e09
DELL <DELL@SEMIH>
2022-04-07 00:10:08 +0300

    HttpGet HttpPost Register Operations

---
+++ CoreMvcProjectDemo/Controllers/RegisterController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMvcProjectDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet] //Ekleme işlemi gerçekleştirirken http post kullan.
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost] //Http get sayfa yüklenince çalışır. Http post sayfada buton tetiklenince çalışır.
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
