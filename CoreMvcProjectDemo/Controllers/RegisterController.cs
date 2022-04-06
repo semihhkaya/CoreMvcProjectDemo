8139fdc95eb0cf0d8da3a272bc0f0be5069085e1
DELL <DELL@SEMIH>
2022-04-07 00:32:13 +0300

    Register Controller, SweetAlert, WriterService, HttpPost, HttpGet

--- CoreMvcProjectDemo/Controllers/RegisterController.cs
+++ CoreMvcProjectDemo/Controllers/RegisterController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +11,20 @@ namespace CoreMvcProjectDemo.Controllers
 {
     public class RegisterController : Controller
     {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+
         [HttpGet] //Ekleme işlemi gerçekleştirirken http post kullan.
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost] //Http get sayfa yüklenince çalışır. Http post sayfada buton tetiklenince çalışır.
-        public IActionResult Index()
+        public IActionResult Index(Writer p)
         {
-            return View();
+            p.WriterStatus = true;
+            p.WriterAbout = "Deneme Test";
+            wm.WriterAdd(p);
+            return RedirectToAction("Index", "Blog");
         }
     }
 }
