c2b1d161d99ba02eb69720bd6a77bcd45a0756f6
DELL <DELL@SEMIH>
2022-04-07 21:54:53 +0300

    Fluent Validation Core, Validation Rules.

--- CoreMvcProjectDemo/Controllers/RegisterController.cs
+++ CoreMvcProjectDemo/Controllers/RegisterController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -21,10 +23,24 @@ namespace CoreMvcProjectDemo.Controllers
         [HttpPost] //Http get sayfa yüklenince çalışır. Http post sayfada buton tetiklenince çalışır.
         public IActionResult Index(Writer p)
         {
-            p.WriterStatus = true;
-            p.WriterAbout = "Deneme Test";
-            wm.WriterAdd(p);
-            return RedirectToAction("Index", "Blog");
+            WriterValidator wv = new WriterValidator(); //Validasyonların kontrolü
+            ValidationResult results = wv.Validate(p);
+            if (results.IsValid)
+            {
+                p.WriterStatus = true;
+                p.WriterAbout = "Deneme Test";
+                wm.WriterAdd(p);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
         }
     }
 }
