02578a8c09fba0c25f5d330e579329ec374aa128
DELL <DELL@SEMIH>
2022-10-28 15:28:03 +0300

    Ajax, Jquery, Scripts.

---
+++ CoreMvcProjectDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,40 @@
+﻿using CoreMvcProjectDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMvcProjectDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+            
+            list.Add(new CategoryClass { 
+                categoryName="Teknoloji",
+                categoryCount = 10
+            });
+
+            list.Add(new CategoryClass
+            {
+                categoryName = "Yazılım",
+                categoryCount = 10
+            });
+
+            list.Add(new CategoryClass
+            {
+                categoryName = "Spor",
+                categoryCount = 10
+            });
+            return Json(new { jsonlist = list });
+        }
+    }
+}
---
+++ CoreMvcProjectDemo/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,50 @@
+﻿using CoreMvcProjectDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMvcProjectDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+        public IActionResult GetWriterById(int writerid)
+        {
+            var findWriter = writers.FirstOrDefault(x => x.Id == writerid);
+            var jsonWriters = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriters);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass
+            {
+                Id=1,
+                Name="Semih"
+            },
+            new WriterClass
+            {
+                Id=2,
+                Name="Deneme"
+            },
+            new WriterClass
+            {
+                Id=3,
+                Name="Okey"
+            }
+        };
+    }
+}
