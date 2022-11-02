dc7199f73708081008a256ee1608292bebe93faa
DELL <DELL@SEMIH>
2022-11-02 13:09:05 +0300

    .

--- CoreMvcProjectDemo/Areas/Admin/Controllers/WriterController.cs
+++ CoreMvcProjectDemo/Areas/Admin/Controllers/WriterController.cs
@@ -28,6 +28,14 @@ namespace CoreMvcProjectDemo.Areas.Admin.Controllers
             return Json(jsonWriters);
         }
 
+        [HttpPost]
+        public IActionResult AddWriter(WriterClass w)
+        {
+            writers.Add(w);
+            var jsonWriters = JsonConvert.SerializeObject(w);
+            return Json(jsonWriters);
+        }
+
         public static List<WriterClass> writers = new List<WriterClass>
         {
             new WriterClass
